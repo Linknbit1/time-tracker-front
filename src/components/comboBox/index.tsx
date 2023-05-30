c6882a3e87893fa821af378473abbcfe274870b5
zain <zainmalik080896@gmail.com>
2023-05-30 10:20:35 +0500

    font size adjusted

--- src/components/comboBox/index.tsx
+++ src/components/comboBox/index.tsx
@@ -1,15 +1,38 @@
 import Autocomplete from "@mui/material/Autocomplete";
+import { styled } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import * as React from "react";
 
 export default function ComboBox() {
+  const CustomTextField = styled(TextField)({
+    "& .MuiInputBase-root": {
+      fontSize: "20px",
+      border: "2px solid red"
+    }
+  });
+
   return (
     <Autocomplete
       disablePortal
       id="combo-box-demo"
       options={top100Films}
-      sx={{ width: 540 }}
-      renderInput={params => <TextField {...params} label="PROJECT*" size="medium" placeholder="Projects" />}
+      sx={{
+        width: 540
+      }}
+      renderInput={params => (
+        <TextField
+          {...params}
+          label="PROJECT*"
+          size="medium"
+          placeholder="Projects"
+          sx={{
+            "& .MuiTextField-root": {
+              fontSize: "20px",
+              border: "2px solid red"
+            }
+          }}
+        />
+      )}
     />
   );
 }
