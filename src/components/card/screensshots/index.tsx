6a39d32291295989dae285c81786296802a522c9
zain <zainmalik080896@gmail.com>
2023-06-23 00:38:55 +0500

    worked on no activity svg visibility

--- src/components/card/screensshots/index.tsx
+++ src/components/card/screensshots/index.tsx
@@ -1,8 +1,10 @@
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditIcon from "@mui/icons-material/Edit";
+import PanoramaFishEyeBorderIcon from "@mui/icons-material/PanoramaFishEye";
 import PermMediaIcon from "@mui/icons-material/PermMedia";
 import TurnRightIcon from "@mui/icons-material/TurnRight";
-import { Box, Button, Fade, Tooltip } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -15,21 +17,6 @@ import * as React from "react";
 
 import ScreenShotImage from "../../../assets/png/Screenshot 2023-06-20 at 12.12.50 PM.png";
 
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest
-  })
-}));
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 4,
   borderRadius: 5,
@@ -41,120 +28,162 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "light" ? "#ffb10b" : "#308fe8"
   }
 }));
-const ScreenShotsCard = () => {
-  const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+interface Props {
+  data: {
+    id: number;
+    startTime: number;
+    endTime: number;
+    activity: string;
   };
+}
+
+const ScreenShotsCard = ({ data }: Props) => {
+  const startTime = new Date(data.startTime).toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true
+  });
+  const endTime = new Date(data.endTime).toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true
+  });
+  console.log(startTime);
 
   return (
-    <Card sx={{ width: "200px", boxShadow: "none !important", border: "1px solid #e4e9ef" }}>
+    <Box sx={{ width: "244px", display: "flex", flexDirection: "column", alignItems: "center", mt: 2 }}>
       <Box
         sx={{
-          position: "relative",
-          overflow: "hidden", // Ensures the hidden content doesn't overflow
-
-          "&:hover": {
-            "& .css-sw3gy7": {
-              display: "block !important",
-              transform: "translateY(0)" // Moves the element from top to bottom
-            }
-          }
+          display: "inline-block",
+          justifyContent: "center",
+          backgroundColor: "#eff1f5",
+          borderRadius: "20px",
+          maxWidth: "100%",
+          minHeight: "24px",
+          padding: "3px 12px"
         }}
       >
-        <CardMedia component="img" height="112" image={ScreenShotImage} alt="Paella dish" />
-        <Box
-          sx={{
-            position: "absolute",
-            top: 0,
-            border: "2px solid blue",
-            height: "100%",
-            width: "100%",
-            display: "none",
-            backgroundColor: "rgba(0,0,0,0.65)",
-            transform: "translateY(-100%)", // Initial position above the element
-            transition: "transform 0.3s ease"
-          }}
-          className="css-sw3gy7"
+        <Typography
+          variant="h5"
+          sx={{ display: "inline-block", textOverflow: "ellipsis", color: "#3e4956", fontSize: "13px", fontWeight: "600", margin: "0 auto" }}
         >
-          <Box sx={{ display: "flex", justifyContent: "space-evenly", alignItems: "center" }}>
-            <Checkbox
-              sx={{
-                color: "#4ccead"
-              }}
-            />
-            <PermMediaIcon fontSize="medium" color="secondary" />
-            <TurnRightIcon fontSize="medium" color="secondary" />
-            <DeleteForeverIcon fontSize="medium" color="secondary" />
-          </Box>
-          <Box>
-            <Button variant="contained" color="secondary" sx={{ ml: 5, mt: 2 }}>
-              View Screens
-            </Button>
-          </Box>
-        </Box>
+          Time Tracker
+        </Typography>
       </Box>
-
-      <CardContent sx={{ position: "relative" }}>
-        <Typography variant="body1" color="text.secondary" sx={{ display: "flex", justifyContent: "space-between", mt: 1 }}>
-          9:10 am - 9:20 am <EditIcon fontSize="small" color="info" />
-        </Typography>
-        <BorderLinearProgress variant="determinate" value={50} sx={{ mt: 1 }} />
-        <Typography variant="body2" color="text.secondary" sx={{ display: "flex", justifyContent: "center", mt: 1 }}>
-          2% of 7 minutes
-        </Typography>
+      <Typography variant="h5" sx={{ textOverflow: "ellipsis", color: "#b0b9c4 !important", fontSize: "12px", fontWeight: "500", mt: 1, mb: 1 }}>
+        No to-dos
+      </Typography>
+      <Card sx={{ width: "100%", boxShadow: "none !important", border: "1px solid #e4e9ef" }}>
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            background: "#fff",
-            borderRadius: "20px",
-            boxShadow: "0 2px 4px rgba(0,0,0,0.19)",
+            position: "relative",
+            overflow: "hidden", // Ensures the hidden content doesn't overflow
 
-            position: "absolute",
-            margin: "-18 auto 0",
-            padding: "5px 15px",
-            top: "-15px",
-            left: "25%"
+            "&:hover": {
+              "& .css-sw3gy7": {
+                transform: "translateY(0)"
+              }
+            }
           }}
         >
-          <Typography variant="h5" sx={{ color: "#2aa7ff", fontSize: "12px", fontWeight: "bold" }}>
-            3 screens
-          </Typography>
-        </Box>
-      </CardContent>
+          <CardMedia
+            component="img"
+            height="135"
+            sx={{ aspectRatio: "5 / 3" }}
+            // image={`https://picsum.photos/40?random=${data.id}`}
+            image={ScreenShotImage}
+            alt="Paella dish"
+          />
+          <Box
+            sx={{
+              position: "absolute",
+              top: 0,
+              height: "100%",
+              width: "100%",
+              backgroundColor: "rgba(0,0,0,0.65)",
 
-      {/* <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore expand={expanded} onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more">
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes.</Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken and chorizo
-            in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook, stirring often until thickened and fragrant,
-            about 10 minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+              transform: "translateY(-100%)",
+              transition: "transform 0.1s ease"
+            }}
+            className="css-sw3gy7"
+          >
+            <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", color: "#fff", padding: "3px 10px" }}>
+              <Checkbox
+                icon={<PanoramaFishEyeBorderIcon sx={{ color: "#fff" }} />}
+                checkedIcon={
+                  <CheckCircleOutlineIcon
+                    sx={{
+                      "& path": {
+                        fill: "#2aa7ff"
+                      },
+                      color: "#fff"
+                    }}
+                  />
+                }
+              />
+
+              <Box>
+                <PermMediaIcon sx={{ mr: 1 }} fontSize="small" />
+                <TurnRightIcon sx={{ mr: 1 }} fontSize="small" />
+                <DeleteForeverIcon fontSize="small" />
+              </Box>
+            </Box>
+            <Box>
+              <Button
+                variant="contained"
+                sx={{
+                  margin: "0 auto",
+                  mt: 2,
+                  display: "block",
+                  backgroundColor: "#fff",
+                  color: "#2b343f",
+                  textTransform: "none !important",
+                  fontSize: "12px",
+                  fontWeight: "500",
+                  "&:hover": {
+                    backgroundColor: "#fffff2",
+                    color: "#2b343f"
+                  }
+                }}
+              >
+                View Screens
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+
+        <CardContent sx={{ position: "relative" }}>
+          <Typography variant="body1" color="text.secondary" sx={{ display: "flex", justifyContent: "space-between", mt: 1 }}>
+            {startTime} - {endTime} <EditIcon fontSize="small" color="info" />
           </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender, 5 to 7 minutes more. (Discard any mussels that don&apos;t open.)
+          <BorderLinearProgress variant="determinate" value={50} sx={{ mt: 1 }} />
+          <Typography variant="body2" color="text.secondary" sx={{ display: "flex", justifyContent: "center", mt: 1 }}>
+            {data.activity}
           </Typography>
-          <Typography>Set aside off of the heat to let rest for 10 minutes, and then serve.</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              background: "#fff",
+              borderRadius: "20px",
+              boxShadow: "0 2px 4px rgba(0,0,0,0.19)",
+
+              position: "absolute",
+              margin: "0 auto",
+              padding: "5px 15px",
+              top: "-15px",
+              left: "50%",
+              transform: "translateX(-50%)"
+            }}
+          >
+            <Typography variant="h5" sx={{ color: "#2aa7ff", fontSize: "12px", fontWeight: "bold" }}>
+              3 screens
+            </Typography>
+          </Box>
         </CardContent>
-      </Collapse> */}
-    </Card>
+      </Card>
+    </Box>
   );
 };
 
--- src/pages/screenShots/index.tsx
+++ src/pages/screenShots/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
+import Link from "@mui/material/Link";
 import TextField from "@mui/material/TextField";
 import React from "react";
 
@@ -16,6 +17,129 @@ const ScreenShots = () => {
     { label: "Saif Ullah" },
     { label: "Faizan" },
     { label: "Muneeb" }
+  ];
+
+  const screenShotsData = [
+    {
+      id: 1,
+      startTime: new Date().setHours(0, 50, 0, 0),
+      endTime: new Date().setHours(1, 20, 0, 0),
+      activity: "2% of 7 minutes"
+    },
+    {
+      id: 2,
+      startTime: new Date().setHours(13, 0, 0, 0),
+      endTime: new Date().setHours(14, 0, 0, 0),
+      activity: "80% of 10 minutes"
+    },
+    {
+      id: 3,
+      startTime: new Date().setHours(9, 15, 0, 0),
+      endTime: new Date().setHours(9, 30, 0, 0),
+      activity: "10% of 15 minutes"
+    },
+    {
+      id: 4,
+      startTime: new Date().setHours(3, 45, 0, 0),
+      endTime: new Date().setHours(4, 0, 0, 0),
+      activity: "20% of 10 minutes"
+    },
+    {
+      id: 5,
+      startTime: new Date().setHours(7, 30, 0, 0),
+      endTime: new Date().setHours(8, 0, 0, 0),
+      activity: "8% of 20 minutes"
+    },
+    {
+      id: 6,
+      startTime: new Date().setHours(15, 45, 0, 0),
+      endTime: new Date().setHours(16, 0, 0, 0),
+      activity: "25% of 15 minutes"
+    },
+    {
+      id: 7,
+      startTime: new Date().setHours(11, 0, 0, 0),
+      endTime: new Date().setHours(11, 30, 0, 0),
+      activity: "12% of 30 minutes"
+    },
+    {
+      id: 8,
+      startTime: new Date().setHours(5, 20, 0, 0),
+      endTime: new Date().setHours(5, 40, 0, 0),
+      activity: "4% of 20 minutes"
+    },
+    {
+      id: 9,
+      startTime: new Date().setHours(18, 15, 0, 0),
+      endTime: new Date().setHours(18, 45, 0, 0),
+      activity: "15% of 30 minutes"
+    },
+    {
+      id: 10,
+      startTime: new Date().setHours(20, 0, 0, 0),
+      endTime: new Date().setHours(20, 30, 0, 0),
+      activity: "50% of 60 minutes"
+    },
+    {
+      id: 11,
+      startTime: new Date().setHours(2, 0, 0, 0),
+      endTime: new Date().setHours(2, 15, 0, 0),
+      activity: "15% of 15 minutes"
+    },
+    {
+      id: 12,
+      startTime: new Date().setHours(12, 45, 0, 0),
+      endTime: new Date().setHours(13, 0, 0, 0),
+      activity: "5% of 15 minutes"
+    },
+    {
+      id: 13,
+      startTime: new Date().setHours(6, 0, 0, 0),
+      endTime: new Date().setHours(6, 30, 0, 0),
+      activity: "10% of 30 minutes"
+    },
+    {
+      id: 14,
+      startTime: new Date().setHours(10, 20, 0, 0),
+      endTime: new Date().setHours(10, 45, 0, 0),
+      activity: "30% of 25 minutes"
+    },
+    {
+      id: 15,
+      startTime: new Date().setHours(17, 30, 0, 0),
+      endTime: new Date().setHours(17, 45, 0, 0),
+      activity: "10% of 15 minutes"
+    },
+    {
+      id: 16,
+      startTime: new Date().setHours(21, 0, 0, 0),
+      endTime: new Date().setHours(21, 15, 0, 0),
+      activity: "25% of 15 minutes"
+    },
+    {
+      id: 17,
+      startTime: new Date().setHours(4, 30, 0, 0),
+      endTime: new Date().setHours(5, 0, 0, 0),
+      activity: "10% of 30 minutes"
+    },
+    {
+      id: 18,
+      startTime: new Date().setHours(8, 45, 0, 0),
+      endTime: new Date().setHours(9, 15, 0, 0),
+      activity: "20% of 30 minutes"
+    },
+    {
+      id: 19,
+      startTime: new Date().setHours(14, 45, 0, 0),
+      endTime: new Date().setHours(15, 0, 0, 0),
+      activity: "5% of 15 minutes"
+    },
+    {
+      id: 20,
+      startTime: new Date().setHours(19, 30, 0, 0),
+      endTime: new Date().setHours(20, 0, 0, 0),
+      activity: "10% of 30 minutes"
+    }
   ];
 
   return (
@@ -47,9 +171,22 @@ const ScreenShots = () => {
           <Typography sx={{ fontSize: "31px", fontWeight: "300", color: "#2b343f", pt: 5, pl: 2 }}>23%</Typography>
         </Box>
       </Box>
-      <Box sx={{ mt: 4 }}>
-        <ScreenShotsCard />
+      <Box sx={{ mt: 4, justifyContent: "center", display: "flex", flexWrap: "wrap", gap: "20px" }}>
+        {screenShotsData ? (
+          screenShotsData.map((item, idx) => <ScreenShotsCard key={idx} data={item} />)
+        ) : (
+          <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 1 }}>
+            <img src="https://server-assets.hubstaff.com/4e1885b5dc/vite/no-activity.379c2c78.svg" width={420} height={360} />
+            <Typography variant="h4" sx={{ fontWeight: 600 }}>
+              There isn't any activity tracked yet for this day. To track time{" "}
+              <Link sx={{ color: "#2aa7ff", textDecoration: "none" }} href="https://app.hubstaff.com/download">
+                use one of our apps.
+              </Link>
+            </Typography>
+          </Box>
+        )}
       </Box>
+      ;
     </div>
   );
 };
